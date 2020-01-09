9b24df755a95c528e4305c05c6fd6430f465f312
Vishwas Shukla <vshuk003@wch136-16.cs.ucr.edu>
2020-01-09 15:00:40 -0800

    Simulating

---
+++ source/main.c
@@ -0,0 +1,30 @@
+/*	Author: vshuk003
+ *  Partner(s) Name: Nicole Nguyen
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRB = 0xFF;
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	PORTB = 0x00;
+	
+	unsigned char temp_input = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	temp_input = PINA;
+	PORTB = temp_input;
+    }
+
+    return 1;
+}
